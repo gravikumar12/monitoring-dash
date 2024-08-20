45bfe207cd6aacbd459cad50fda47f4429c2ffbc
Ravi kumar <119988156+gravikumar12@users.noreply.github.com>
2024-08-20 08:32:59 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,27 +1,58 @@
 resource "google_monitoring_dashboard" "bq_dashboard" {
   dashboard_json = <<EOF
 {
-  "displayName": "BigQuery Rows Inserted",
-  "widgets": [
-    {
-      "title": "Rows Inserted into BigQuery",
-      "xyChart": {
-        "dataSets": [
-          {
-            "timeSeriesQuery": {
-              "timeSeriesFilter": {
-                "filter": "metric.type=\"logging.googleapis.com/user/bigquery_row_inserts\"",
-                "aggregation": {
-                  "alignmentPeriod": "60s",
-                  "perSeriesAligner": "ALIGN_RATE"
+  "displayName": "BigQuery Monitoring Dashboard",
+  "gridLayout": {
+    "columns": 2,
+    "widgets": [
+      {
+        "title": "BigQuery Load Errors",
+        "xyChart": {
+          "dataSets": [
+            {
+              "timeSeriesQuery": {
+                "timeSeriesFilter": {
+                  "filter": "metric.type=\"logging.googleapis.com/user/bigquery_load_errors\"",
+                  "aggregation": {
+                    "alignmentPeriod": "60s",
+                    "perSeriesAligner": "ALIGN_DELTA"
+                  }
                 }
-              }
+              },
+              "plotType": "LINE"
             }
+          ],
+          "yAxis": {
+            "label": "Error Count",
+            "scale": "LINEAR"
           }
-        ]
+        }
+      },
+      {
+        "title": "Rows Inserted into BigQuery Dataset",
+        "xyChart": {
+          "dataSets": [
+            {
+              "timeSeriesQuery": {
+                "timeSeriesFilter": {
+                  "filter": "metric.type=\"logging.googleapis.com/user/bigquery_row_inserts\"",
+                  "aggregation": {
+                    "alignmentPeriod": "60s",
+                    "perSeriesAligner": "ALIGN_DELTA"
+                  }
+                }
+              },
+              "plotType": "LINE"
+            }
+          ],
+          "yAxis": {
+            "label": "Row Count",
+            "scale": "LINEAR"
+          }
+        }
       }
-    }
-  ]
+    ]
+  }
 }
 EOF
 }
